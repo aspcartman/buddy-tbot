fe6f10479df8e8cfdee78efbbd321e5f89455713
Ruslan Fedorov <ruslan@tapingo.com>
2017-12-21 03:19:04 +0200

    Fix makefile and main

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/aspcartman/buddy-tbot/api"
 	"github.com/aspcartman/buddy-tbot/bot"
 	"github.com/sirupsen/logrus"
@@ -13,4 +15,6 @@ func main() {
 	api.ListenForAPIEvents(func() {
 		b.SendNotification()
 	})
+
+	time.Sleep(1 * time.Hour)
 }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 
 build: build/buddy_bot
 
-build/buddy_bot: vendor
+build/buddy_bot: vendor $(shell find . -type f -name *.go)
 	mkdir -pv build
 	go build -o build/buddy_bot main.go
 
